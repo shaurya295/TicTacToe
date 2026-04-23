93e517e254092e1d36b8bae2fbec58b6fca1643d
shaurya295 <shauryakakkar03@gmail.com>
2026-04-23 22:07:43 +0530

    Added UC2

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Random;
+
 public class TicTacToe {
 
+    // Initialize board
     public static void initializeBoard(char[][] board) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -10,6 +13,7 @@ public class TicTacToe {
         }
     }
 
+    // Print board
     public static void printBoard(char[][] board) {
         System.out.println("Tic-Tac-Toe Board:");
         for (int i = 0; i < 3; i++) {
@@ -20,10 +24,39 @@ public class TicTacToe {
         }
     }
 
+    // UC2: Toss logic
+    public static void tossAndAssign() {
+        Random rand = new Random();
+
+        int toss = rand.nextInt(2); // 0 or 1
+
+        char userSymbol;
+        char computerSymbol;
+        String currentPlayer;
+
+        if (toss == 0) {
+            userSymbol = 'X';
+            computerSymbol = 'O';
+            currentPlayer = "User";
+        } else {
+            userSymbol = 'O';
+            computerSymbol = 'X';
+            currentPlayer = "Computer";
+        }
+
+        System.out.println("Toss Result:");
+        System.out.println("User Symbol: " + userSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+        System.out.println("First Turn: " + currentPlayer);
+    }
+
     public static void main(String[] args) {
         char[][] board = new char[3][3];
 
         initializeBoard(board);
         printBoard(board);
+
+        // UC2 Execution
+        tossAndAssign();
     }
 }
