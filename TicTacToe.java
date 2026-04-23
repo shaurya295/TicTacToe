cca35727ccb7a77184309a8712f4ba89a98adbdd
shaurya295 <shauryakakkar03@gmail.com>
2026-04-23 22:09:50 +0530

    Added UC4

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,7 +7,12 @@ public class TicTacToe {
 
     static Scanner scanner = new Scanner(System.in);
 
-    // Initialize board
+    // Game state variables
+    static char userSymbol;
+    static char computerSymbol;
+    static String currentPlayer;
+
+    // UC1: Initialize board
     public static void initializeBoard(char[][] board) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -16,9 +21,9 @@ public class TicTacToe {
         }
     }
 
-    // Print board
+    // UC1: Print board
     public static void printBoard(char[][] board) {
-        System.out.println("Tic-Tac-Toe Board:");
+        System.out.println("\nTic-Tac-Toe Board:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
@@ -27,34 +32,62 @@ public class TicTacToe {
         }
     }
 
-    // UC2: Toss
+    // UC2: Toss and assign symbols
     public static void tossAndAssign() {
         Random rand = new Random();
         int toss = rand.nextInt(2);
 
         if (toss == 0) {
-            System.out.println("User plays first with X");
+            userSymbol = 'X';
+            computerSymbol = 'O';
+            currentPlayer = "User";
         } else {
-            System.out.println("Computer plays first with X");
+            userSymbol = 'O';
+            computerSymbol = 'X';
+            currentPlayer = "Computer";
         }
+
+        System.out.println("\n--- Toss Result ---");
+        System.out.println("User Symbol: " + userSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+        System.out.println("First Turn: " + currentPlayer);
     }
 
-    // UC3: Get user slot input
+    // UC3: Get user input
     public static int getUserInput() {
-        System.out.print("Enter a slot number (1-9): ");
+        System.out.print("\nEnter a slot number (1-9): ");
         int slot = scanner.nextInt();
         return slot;
     }
 
+    // UC4: Convert slot to row & column
+    public static int[] convertSlotToIndex(int slot) {
+        int index = slot - 1;   // Convert to 0-based
+        int row = index / 3;
+        int col = index % 3;
+
+        return new int[]{row, col};
+    }
+
     public static void main(String[] args) {
+
         char[][] board = new char[3][3];
 
+        // UC1
         initializeBoard(board);
         printBoard(board);
+
+        // UC2
         tossAndAssign();
 
-        // UC3 Execution
+        // UC3
         int userSlot = getUserInput();
-        System.out.println("You selected slot: " + userSlot);
+
+        // UC4
+        int[] position = convertSlotToIndex(userSlot);
+
+        System.out.println("\nConverted Position:");
+        System.out.println("Row: " + position[0]);
+        System.out.println("Column: " + position[1]);
     }
 }
